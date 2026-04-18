5002d7e01aaf3fa557fe4de76a1c381999716e3c
Manoj Kumar Mandava <manojmandava9894@gmail.com>
2026-04-18 09:24:46 -0600

    Add Supabase config

---
+++ js/supabase-config.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = "https://chviulgpmqiywadrbzxt.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_44X6pu3BNfHIGYDEVZO3Fg_KrHQypd0";
+
+const supabaseClient = window.supabase.createClient("https://chviulgpmqiywadrbzxt.supabase.co", "sb_publishable_44X6pu3BNfHIGYDEVZO3Fg_KrHQypd0");
+
+window.supabaseClient = supabaseClient;

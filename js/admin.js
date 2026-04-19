1a363c45752b2adfc737e3f74642cd8fcc7907c4
Manoj Kumar Mandava <manojmandava9894@gmail.com>
2026-04-19 09:50:49 -0600

    Add text alignment to blog editor

--- js/admin.js
+++ js/admin.js
@@ -213,7 +213,9 @@ function initQuill() {
                 [{ header: [1, 2, 3, false] }],
                 ['bold', 'italic', 'underline', 'strike'],
                 [{ color: [] }, { background: [] }],
+                [{ align: [] }],
                 [{ list: 'ordered' }, { list: 'bullet' }],
+                [{ indent: '-1' }, { indent: '+1' }],
                 ['blockquote', 'code-block'],
                 ['link', 'image'],
                 ['clean']
